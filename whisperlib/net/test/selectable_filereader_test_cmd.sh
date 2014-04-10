6b61ae7d0f5eb92f2bf26c7f4f8cce26bfffcd80
Ovidiu Predescu <ovidiu@gmail.com>
2014-04-10 09:11:07 -0700

    Create a shell script driver for selectable_filereader_test to be run by make.

---
+++ whisperlib/net/test/selectable_filereader_test_cmd.sh
@@ -0,0 +1,3 @@
+# Test driver for whisperlib/net/test/selectable_filereader_test.
+
+whisperlib/net/test/selectable_filereader_test --cmd_path=whisperlib/net/test/selectable_filereader_test.sh --expected_bytes=27059
